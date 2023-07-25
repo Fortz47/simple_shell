490d171d9ffa601b73e21819027ae590c23c462a
Fortz47 <65767694+Fortz47@users.noreply.github.com>
2023-07-25 18:58:03 +0100

    Update utils2.c

--- simple_shell/utils2.c
+++ simple_shell/utils2.c
@@ -55,3 +55,63 @@ char **rm_env(char **env, int index)
 	envCopy[j] = NULL;
 	return (envCopy);
 }
+
+/**
+ * then_free - frees memory
+ * @count: number of argument to free
+ * @_return: return value
+ * @ptr: pointer to struct parse
+ *
+ * Return: return value
+ */
+parse *then_free(int count, parse *_return, parse *ptr, ...)
+{
+	char *str;
+	char **str_arr;
+	va_list list;
+
+	va_start(list, ptr);
+
+	while (count)
+	{
+		str = va_arg(list, char *);
+		free(str);
+		count--;
+	}
+	str_arr = va_arg(list, char **);
+	if (str_arr)
+		free(str_arr);
+	if (ptr)
+		free(ptr);
+	va_end(list);
+	return (_return);
+}
+
+/**
+ * _then_free - frees memory
+ * @count: number of argument to free
+ * @ptr: pointer to struct parse
+ *
+ * Return: return value
+ */
+void _then_free(int count, parse *ptr, ...)
+{
+	char *str;
+	char **str_arr;
+	va_list list;
+
+	va_start(list, ptr);
+
+	while (count)
+	{
+		str = (va_arg(list, char *));
+		free(str);
+		count--;
+	}
+	str_arr = va_arg(list, char **);
+	if (str_arr)
+		free(str_arr);
+	if (ptr)
+		free(ptr);
+	va_end(list);
+}
